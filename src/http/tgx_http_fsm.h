36b9a697d08bce88ae5f7e3f76224651479bdf90
van9ogh <van9ogh@gmail.com>
2012-09-19 22:23:04 +0800

    rewrite tgx_connection.c and delete tgx_http_fsm.c/.h

--- src/http/tgx_http_fsm.h
+++ src/http/tgx_http_fsm.h
@@ -3,20 +3,14 @@
 #include "tgx_setting.h"
 #include "tgx_core.h"
 
-#define TGX_STATUS_CONNECT								1
 
-#define TGX_STATUS_READ_REQUEST_HEADER					2
-#define TGX_STATUS_PARSING_REQUEST_HEADER				3
+#define TGX_STATUS_READ_REQUEST
+#define TGX_STATUS_PARSING_REQUEST
+#define TGX_STATUS_SEND_RESPONSE
 
-#define TGX_STATUS_GET_SEND_RESPONSE_HEADER				4
-#define TGX_STATUS_GET_SEND_RESPONSE_FILE				5
+// 一旦发生错误无条件close
+#define TGX_STATUS_CLOSE
 
-#define TGX_STATUS_POST_READ_REQUEST_MESSAGE_BODY		6
-#define TGX_STATUS_POST_PARSING_REQUEST_MESSAGE_BODY	7
-#define TGX_STATUS_POST_SEND_RESPONSE_HEADER			8	
-#define TGX_STATUS_POST_SEND_RESPONSE_DATA				9
-
-#define TGX_STATUS_ERROR								10
-#define TGX_STATUS_CLOSE								11
-void tgx_http_fsm_set_status(tgx_connection_t *tc, int status);
-int tgx_http_fsm_state_machine(tgx_cycle_t *tcycle, tgx_connection_t *tc);
+// 这个状态可以支持keep-alive, 在send response完成后
+// 可以不关闭connection
+#define TGX_STATUS_KEEPALIVE
