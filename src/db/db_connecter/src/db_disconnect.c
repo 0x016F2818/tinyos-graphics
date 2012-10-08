3629ab1cf58cc8d520f2d9aac805231648340921
arnold <jq.arnold.li@gmail.com>
2012-10-08 22:31:44 +0800

    delete again

--- src/db/db_connecter/src/db_disconnect.c
+++
@@ -1,7 +0,0 @@
-#include "../include/sensor.h"
-void db_disconnect(db_connect_t *db_connector){
-
-    mysql_close((MYSQL *)(db_connector->db_handler));
-    free(db_connector->db_handler);
-}
-
--- www/js/temperature.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    // drawSpline('temperature');
-    // drawSpline('container');
-    drawDynamicSpline();
-    drawSymbolsSpline();
-    drawRotatedColumn();
-    drawMaxandMinColumn();
-    drawGradientPie();
-})
