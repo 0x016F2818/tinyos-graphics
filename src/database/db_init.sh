548fbfedce3638734c4e9dab8b4d06f577a6b3b1
arnold <jq.arnold.li@gmail.com>
2012-10-08 21:51:53 +0800

    perfect the db_init.sh

--- src/database/db_init.sh
+++ src/database/db_init.sh
@@ -6,19 +6,18 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-echo "Making sure the basic database exists:"
-mysql -u root -p < ./db_sql/basic_tinyos_db.sql
-echo "Initializing the database..."
-cat ./db_sql/table_creater.sql ./db_sql/view_creater.sql ./db_sql/insert_proc_creater.sql\
-    ./db_sql/search_proc_creater.sql | mysql -u root -p tinyos
+host=127.0.0.1
+user=root
+password=arnold
+db_name=tinyos
 
 #read -p "Please input the db_host: " host  # 提示输入数据库服务器地址
 #read -p "Please input your db_user:  " user   # 输入数据库用户名
 #read -p "Please input your password:  " password   # 输入用户密码
 #read -p "Please input your db_name:  " db_name   # 输入数据库名
 
-#echo "Make sure the basic database exists:"
-#echo "create database if not exists $db_name" | mysql -u $user -h $host -p $password 
-#echo "Initialize the database..."
-#cat ./db_sql/table_creater.sql ./db_sql/view_creater.sql ./db_sql/insert_proc_creater.sql\
-    #./db_sql/search_proc_creater.sql | mysql -u root -p $db_name
+echo "Making sure the basic database exists:"
+echo "create database if not exists $db_name" | mysql -u $user --password=$password -h $host
+echo "Initializing the database..."
+cat ./db_sql/table_creater.sql ./db_sql/view_creater.sql ./db_sql/insert_proc_creater.sql\
+    ./db_sql/search_proc_creater.sql | mysql -u $user --password=$password -h $host $db_name
